0e346c3d900d325ac0d3ced9911010237ce1d3ad
Vanesa Korbenfeld <vkorbenfeld@gmail.com>
2022-01-21 17:11:03 -0300

    Initial Commit

---
+++ LayoutWithLazyVarAndPickerDelegate/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LayoutWithLazyVarAndPickerDelegate
+//
+//  Created by Vanesa Korbenfeld on 21/01/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ LayoutWithLazyVarAndPickerDelegateTests/LayoutWithLazyVarAndPickerDelegateTests.swift
@@ -0,0 +1,33 @@
+//
+//  LayoutWithLazyVarAndPickerDelegateTests.swift
+//  LayoutWithLazyVarAndPickerDelegateTests
+//
+//  Created by Vanesa Korbenfeld on 21/01/2022.
+//
+
+import XCTest
+@testable import LayoutWithLazyVarAndPickerDelegate
+
+class LayoutWithLazyVarAndPickerDelegateTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
